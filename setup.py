94f00c0d78135a0b5b638bf6a61559c07ad746e1
MightyPotatoast <chauvet.contact@gmail.com>
2021-08-20 17:07:25 +0200

    img for readme.md, moved the icon

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@
 # python setup.py build
 
 from cx_Freeze import setup, Executable
-  
+
 executables = [
-        Executable(script = "main.py",icon = "discord.ico", base = "Win32GUI" )
+    Executable(script="main.py", icon="/img/discord.ico", base="Win32GUI")
 ]
 # ne pas mettre "base = ..." si le programme n'est pas en mode graphique
-  
-buildOptions = dict( 
-        includes = ["tkinter","ffmpeg"],
-        include_files = ["./bin"]
+
+buildOptions = dict(
+    includes=["tkinter", "ffmpeg"],
+    include_files=["./bin"]
 )
-  
+
 setup(
-    name = "Discpress",
-    version = "1.0",
-    description = "Transcode video files to fit discord weight limit",
-    author = "MightyPotatoast",
-    options = dict(build_exe = buildOptions),
-    executables = executables
+    name="TransCord",
+    version="1.2",
+    description="Transcode video files to fit discord weight limit",
+    author="MightyPotatoast",
+    options=dict(build_exe=buildOptions),
+    executables=executables
 )
